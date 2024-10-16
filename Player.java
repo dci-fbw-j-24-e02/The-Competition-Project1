9591b62e609852feb2a3a545365593ed6e17175c
OlenaShcherbina <olenas.shcherbina@gmail.com>
2024-10-16 14:13:56 +0200

    Added Hangman and currentAttempt as fields and methods connected to them

--- Player.java
+++ Player.java
@@ -1,15 +1,21 @@
 public class Player {
     private String name;
     private int score;
+    private HangmanUI hangman;
+    private int currentAttempt;
 
     public Player(){
         this.name = "New player";
         this.score = 0;
+        this.hangman = new HangmanUI();
+        this.currentAttempt = 0;
     }
 
     public Player(String name){
         this.name = name;
         this.score = score;
+        this.hangman = new HangmanUI();
+        this.currentAttempt = 0;
     }
 
     public String getName(){
@@ -18,10 +24,25 @@ public class Player {
 
     public void increaseScore(){
         score++;
+        updateHangmanStatus();
     }
 
     public int getScore(){
         return score;
+    }
+
+    public void increaseAttempt(){
+        currentAttempt++;
+    }
+
+    public int getCurrentAttempt() { return currentAttempt;}
+
+    public String[] getHangmanStatus(){
+         return this.hangman.getHangman();
+    }
+
+    private void updateHangmanStatus(){
+        this.hangman.updateHangman(this.score);
     }
 
     @Override
